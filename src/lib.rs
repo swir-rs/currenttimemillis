53627b1dd480d36d779c5e3d5c5799d037ed082d
Dawid Nowak <nowakd@gmail.com>
2021-03-15 15:34:53 +0000

    Added panic

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,6 @@ use std::time::{SystemTime};
 pub fn current_time_milliseconds()->u128{
     match SystemTime::from(SystemTime::UNIX_EPOCH).elapsed(){
         Ok(time) => time.as_millis(),
-        Err(_) => 0
+        Err(_) => panic!("SystemTime before UNIX EPOCH!")
     }
 }
